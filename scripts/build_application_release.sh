c2757e717212b4d776067388e1a7b642731ad3c8
Hadan <43314445+nghdang@users.noreply.github.com>
2026-07-19 09:12:45 +0700

    ci: setup build and deploy scripts (#8)
    * ci: setup native flow
    
    * ci: setup build release and deploy

---
+++ scripts/build_application_release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# set -e
+
+THIS_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+
+source "${THIS_DIR}/env.sh"
+
+BUILD_DIR="${PROJECT_DIR}/build-release"
+INSTALL_DIR="${PROJECT_DIR}/build-release/deploy"
+
+OPTION="$1"
+
+dump_env GPP_VERSION CMAKE_VERSION NINJA_BUILD_VERSION BUILD_DIR OPTION | tee "$LOG_PATH"
+
+if [[ "$OPTION" == "dump" ]]
+then
+    exit 0
+fi
+
+if [[ "$OPTION" == "-fn" || "$OPTION" == "-f" ]]
+then
+    rm -rf "$BUILD_DIR"
+    mkdir -p "$BUILD_DIR"
+fi
+
+CMAKE_VARIABLES=("-DCMAKE_BUILD_TYPE=Release")
+
+echo "Running: $CMAKE_EXEC ${CMAKE_VARIABLES[@]} -G Ninja -S \"$PROJECT_DIR\" -B \"$BUILD_DIR\"" | tee -a "$LOG_PATH"
+"$CMAKE_EXEC" ${CMAKE_VARIABLES[@]} -G Ninja -S "$PROJECT_DIR" -B "$BUILD_DIR" 2>&1 | tee -a "$LOG_PATH"
+
+if [[ "$OPTION" != "-fn" && "$OPTION" != "-n" ]]
+then
+    echo "Running: $CMAKE_EXEC --build \"$BUILD_DIR\" --target \"all\" -- -j$(nproc)" | tee -a "$LOG_PATH"
+    "$CMAKE_EXEC" --build "$BUILD_DIR" --target "all" -- -j$(nproc) 2>&1 | tee -a "$LOG_PATH"
+
+    echo "Running: $CMAKE_EXEC --install \"$BUILD_DIR\" --prefix \"$INSTALL_DIR\"" | tee -a "$LOG_PATH"
+    "$CMAKE_EXEC" --install "$BUILD_DIR" --prefix "$INSTALL_DIR" 2>&1 | tee -a "$LOG_PATH"
+fi
