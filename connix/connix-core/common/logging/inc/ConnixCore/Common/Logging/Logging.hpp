eed24967007f059590b097a77ffaffdd94fa2a11
Hadan <43314445+nghdang@users.noreply.github.com>
2026-09-07 21:03:32 +0700

    misc: change folder structure (#14)
    * code: remove legacy code
    
    * arch: remove legacy arch
    
    * misc: restructure project to component-oriented

---
+++ connix/connix-core/common/logging/inc/ConnixCore/Common/Logging/Logging.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cassert>
+
+#include "ConnixCore/Common/Logging/Logger.hpp"
+
+#ifndef LOG_E
+#define LOG_E(...) _LOG(ERROR, __VA_ARGS__)
+#endif
+
+#ifndef LOG_W
+#define LOG_W(...) _LOG(WARN, __VA_ARGS__)
+#endif
+
+#ifndef LOG_I
+#define LOG_I(...) _LOG(INFO, __VA_ARGS__)
+#endif
+
+#ifndef LOG_D
+#define LOG_D(...) _LOG(DEBUG, __VA_ARGS__)
+#endif
+
+#ifndef _LOG
+#define _LOG(level, ...)                                                      \
+    ConnixCore::Common::Logging::Logger::getInstance().record(#level,         \
+                                                              __VA_ARGS__)
+#endif
+
+#ifndef LOG_ASSERT
+#define LOG_ASSERT(condition, ...)                                            \
+    do                                                                        \
+    {                                                                         \
+        if (!(condition))                                                     \
+        {                                                                     \
+            ConnixCore::Common::Logging::Logger::getInstance().record(        \
+                "ASSERT FAILED", __VA_ARGS__);                                \
+            assert(condition);                                                \
+        }                                                                     \
+    } while (0)
+#endif
